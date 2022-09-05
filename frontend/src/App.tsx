4f027add70441e33c277dac1353824ab19b433bd
Lucas Mocroski <devmocroski@gmail.com>
2022-09-05 14:16:41 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import React from "react"
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return(
+    <>
     <h1>Hello world</h1>
-  )
+
+    <NotificationButton/>
+    </> 
+ )
 }
 
 export default App
